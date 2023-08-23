487bb2b0f7e947f0cfbfdfd40d293dfee4a57804
raghav6205 <140968792+raghav6205@users.noreply.github.com>
2023-08-23 12:03:24 +0530

    Add files via upload

---
+++ index.html/script.js
@@ -0,0 +1,21 @@
+const taskInput = document.getElementById('taskInput');
+const addButton = document.getElementById('addButton');
+const taskList = document.getElementById('taskList');
+
+addButton.addEventListener('click', () => {
+  const taskText = taskInput.value.trim();
+  if (taskText !== '') {
+    const taskItem = document.createElement('li');
+    taskItem.innerHTML = `
+      <span>${taskText}</span>
+      <button class="deleteButton">Delete</button>
+    `;
+    taskList.appendChild(taskItem);
+    taskInput.value = '';
+
+    const deleteButton = taskItem.querySelector('.deleteButton');
+    deleteButton.addEventListener('click', () => {
+      taskList.removeChild(taskItem);
+    });
+  }
+});
